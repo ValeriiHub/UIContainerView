bc432ff2890cdc10d07cd6cda4d5321c37d664bf
ValeriyHub <p04taera@gmail.com>
2021-07-02 12:13:47 +0300

    Initial Commit

---
+++ UIContainerView/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  UIContainerView
+//
+//  Created by Valerii D on 02.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imageCover: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
